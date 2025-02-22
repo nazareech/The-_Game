a4d0c9096a50dd308746cefc2df8ed963d1950e2
nazareech <nzrprotasov@gmail.com>
2025-02-22 18:21:13 +0200

    Menu_animation
    Creation animation for main menu

---
+++ Assets/Scripts/Clouds_paralax.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Clouds_paralax : MonoBehaviour
+{
+    [SerializeField] private RawImage _img;
+    [SerializeField] private float _x, _y;
+    
+
+    void Update()
+    {
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size); 
+    }
+}
