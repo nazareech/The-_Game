c4b89108ae82386419fa73cab650e14e4d9bdb24
nazareech <nzrprotasov@gmail.com>
2025-03-10 19:12:37 +0200

    Shooting was made

--- Assets/Scripts/GUN.cs
+++ Assets/Scripts/GUN.cs
@@ -3,18 +3,40 @@ using UnityEngine;
 public class GUN : MonoBehaviour
 {
     [SerializeField] float rotationSpeed;
+    [SerializeField] float spreadAngle = 5f;    // Кут розбігу патронів у градусах
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform shootpos;
+
+    public float fireRate = 0.2f; // Затримка між пострілами
+    private float nextFireTime = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            Shoot();
+        }
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    Shoot();
+        //}    
+
         GunRotation();
     }
+
     void GunRotation()
     {
         Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
@@ -25,4 +47,23 @@ public class GUN : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
 
     }
+
+    void Shoot()
+    {
+
+        // Отримуємо поточний кут стрільби
+        float currentAngle = Mathf.Atan2(shootpos.up.y, shootpos.up.x) * Mathf.Rad2Deg;
+
+        // Додаємо випадкове відхилення в межах розбігу
+        float randomSpread = Random.Range(-spreadAngle, spreadAngle);
+        float newAngle = currentAngle + randomSpread;
+
+        // Створюємо новий кут стрільби
+        Quaternion spreadRotation = Quaternion.Euler(0, 0, newAngle);
+
+        // Створюємо патрон з новим кутом
+        Instantiate(bullet, shootpos.position, spreadRotation);
+        Debug.Log("'Press BaBah!'");
+    }
+
 }
