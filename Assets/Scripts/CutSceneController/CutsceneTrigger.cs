8e9adc622a681d519872c971ce1631902113420d
nazareech <nzrprotasov@gmail.com>
2025-03-21 23:30:43 +0200

    Prototypes for Cutscenes
    Prototypes for two initial cutscenes were created

---
+++ Assets/Scripts/CutSceneController/CutsceneTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Playables; // Потрібно для Timeline
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector cutscene; // Катсцена з Timeline
+    //[SerializeField] private GameObject player; // Гравець, щоб вимкнути керування
+
+    private bool hasPlayed = false; // Щоб не активувати кілька разів
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !hasPlayed)
+        {
+            hasPlayed = true;
+            cutscene.Play(); // Запускаємо катсцену
+           // player.SetActive(false); // Вимикаємо керування гравцем
+           // cutscene.stopped += OnCutsceneEnd; // Додаємо подію на завершення катсцени
+        }
+    }
+
+ /*   private void OnCutsceneEnd(PlayableDirector director)
+    {
+        player.SetActive(true); // Повертаємо керування гравцем
+    }*/
+}
